d3aa94e3b71606bd06680081ba5a4f80dbcf59c2
Thu Vu <tnv2002@nyu.edu>
2024-05-12 01:06:29 -0400

    fix flake8 error

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 # Initialize the Flask application
 app = Flask(__name__)
 
+
 # Home Page
-@app.route('/')
+@app.route("/")
 def home():
     # Render the html file from the templates folder
-    return render_template('home.html', title='Google Suite Manager Tool')
+    return render_template("home.html", title="Google Suite Manager Tool")
 
-@app.route('/home_GoogleMeet')
+
+@app.route("/home_GoogleMeet")
 def home_GoogleMeet():
-    return render_template('home_GoogleMeet.html', title='Google Meet')
+    return render_template("home_GoogleMeet.html", title="Google Meet")
 
-@app.route('/home_GoogleCalendar')
+
+@app.route("/home_GoogleCalendar")
 def home_GoogleCalendar():
-    return render_template('home_GoogleCalendar.html', title='Google Calendar')
+    return render_template("home_GoogleCalendar.html", title="Google Calendar")
 
-@app.route('/sign_in')
+
+@app.route("/sign_in")
 def sign_in():
-    return render_template('sign_in.html', title='Sign in')
+    return render_template("sign_in.html", title="Sign in")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Run the Flask app
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
